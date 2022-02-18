e4edf18e9818946fecda0138100e0620ca3f4160
Steven Furnari <s.furnari@hotmail.com>
2022-02-18 17:54:41 +1100

    model associations complete

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,6 @@
 class Event < ApplicationRecord
+  belongs_to :venue
+  has_many :comments
+  has_many :tickets
+  has_many :users, through: :tickets
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  has_many :comments
+  has_many :tickets
+  has_many :events, through: :tickets
 end
