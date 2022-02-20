714117981536eebff5ac0063039f4ad61c9808b2
danyip <hello@danyip.com>
2022-02-21 00:02:16 +1100

    updated data in event one() api return

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,8 +7,11 @@ class EventsController < ApplicationController
 
   def one
     event = Event.find params[:id]
-
-    render json: event # Add in :include here to grab venue, comment and user images.
+    render json: event, include: [
+                                  {venue: {except: [:updated_at, :created_at]}}, 
+                                  {comments: {include: [user: {only: [:name, :image]}]}}, 
+                                  :tickets #TODO: try and make this return just the number of tickets
+                                ]
   end
 
 end
