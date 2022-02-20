c1e369f69f92bff1bc0da4949267f2a193d09e47
Steven Furnari <s.furnari@hotmail.com>
2022-02-20 14:22:33 +1100

    working on user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,38 @@
 class UsersController < ApplicationController
 
-  before_action :authenticate_user
+  before_action :authenticate_user, except: :create
 
   def current
     render json: current_user
+  end
+
+  def create
+    
+    # binding.pry
+    user = User.new user_params   
+
+    user.save
+
+    render json: user
+
+  end
+
+  def edit
+    
+  end
+
+  def update
+    
+  end
+
+  private
+  def user_params
+    params.permit(
+      :email,
+      :name,
+      :password,
+      :user_level
+    )
   end
   
 end
