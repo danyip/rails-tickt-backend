91c2c180780b4caddd29a1298d7ee7718cce6b23
Steven Furnari <s.furnari@hotmail.com>
2022-02-24 10:19:39 +1100

    edited knock config file

--- config/initializers/knock.rb
+++ config/initializers/knock.rb
@@ -4,7 +4,7 @@ Knock.setup do |config|
 
   config.token_signature_algorithm = 'HS256'
 
-  config.token_secret_signature_key = -> { Rails.application.secrets.secret_key_base }
+  config.token_secret_signature_key = -> { Rails.application.credentials.read }
 
   config.not_found_exception_class_name = 'ActiveRecord::RecordNotFound'
 
